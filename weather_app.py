68a4bdd9ae76481eeeee0b1cb149cdf29310805d
DeborahDelGaudio <delgaudio.d@gmail.com>
2019-05-04 23:39:01 +0200

    clean requests

--- weather_app.py
+++ weather_app.py
@@ -9,19 +9,14 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route('/search', methods=['POST', 'GET'])
-def search_for_a_city():
+@app.route('/result', methods=['POST', 'GET'])
+def result_page():
     if request.method == "POST":
         location = request.form
         w.set_location(location['location'])
-        return redirect(url_for('.result_page', location=location['location']))
+        return render_template('result.html', data=w.get_forecast_data())
     else:
-        return redirect(url_for('.homepage'))
-
-
-@app.route('/weather/<location>')
-def result_page(location):
-    return render_template('result.html', data=w.get_forecast_data())
+        return redirect(url_for('homepage'))
 
 
 if __name__ == '__main__':
